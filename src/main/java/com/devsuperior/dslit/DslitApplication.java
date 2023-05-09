3165013d8bffbb2ff2f9c25b472ebc9b200d6188
willy <willydasilva29@gmail.com>
2023-05-08 22:13:08 -0300

    Project created

---
+++ src/main/java/com/devsuperior/dslit/DslitApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/dslit/DslitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
